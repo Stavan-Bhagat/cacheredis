e201c9fbff853dd9270730d579569ae7573f1a6e
Stavan-Bhagat <126806776+Stavan-Bhagat@users.noreply.github.com>
2024-05-13 14:32:53 +0530

    Update constant.js

--- frontend/src/Constant/constant.js
+++ frontend/src/Constant/constant.js
@@ -16,5 +16,5 @@ export const SET_NAME = (value) => {
   sessionStorage.setItem("name", value);
 };
 export const REACT_PORT = 3000;
-export const BASEURL = "http://localhost:5000/";
+export const BASEURL = "https://blog-site-u6nk.onrender.com/";
 export const passwordEncryptionKey="secretkey123";
