30d86f3c57898327badf388cd45b9f294d399e1f
Stavan-Bhagat <126806776+Stavan-Bhagat@users.noreply.github.com>
2024-05-13 18:20:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ database();
 app.use(express.json());
 // app.use(cors({ origin: ["blog-psi-five-36.vercel.app
 // ", "blog-psi-five-36.vercel.app", "http://localhost:3000", "http://localhost:3000/"] }));
-app.use(cors({ origin: ["https://blog-psi-five-36.vercel.app/","https://blog-psi-five-36.vercel.app"] }));
+app.use(cors({ origin: ["https://blog-psi-five-36.vercel.app","https://blog-psi-five-36.vercel.app/"] }));
 app.use("/submit", userRoutes);
 app.use("/blog", blogRoutes);
 app.use("/refresh", userRoutes);
