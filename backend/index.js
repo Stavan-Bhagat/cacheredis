354055122f0bab30e22df69b901ffe31cde8c27e
Stavan-Bhagat <126806776+Stavan-Bhagat@users.noreply.github.com>
2024-05-14 10:23:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,16 @@ const userRoutes = require("./Routes/allRoutes");
 const blogRoutes = require("./Routes/allRoutes");
 const app = express();
 const port = process.env.PORT;
-const corsOrigin = process.env.CORS_ORIGIN;
 
 database();
 
 app.use(express.json());
-// app.use(cors({ origin: ["blog-psi-five-36.vercel.app
-// ", "blog-psi-five-36.vercel.app", "http://localhost:3000", "http://localhost:3000/"] }));
-app.use(cors({ origin: ["https://blog-psi-five-36.vercel.app","https://blog-psi-five-36.vercel.app/"] }));
+app.use(cors({
+  origin: [
+    "https://blog-psi-five-36.vercel.app",
+    "https://blog-psi-five-36.vercel.app/"
+  ]
+}));
 app.use("/submit", userRoutes);
 app.use("/blog", blogRoutes);
 app.use("/refresh", userRoutes);
